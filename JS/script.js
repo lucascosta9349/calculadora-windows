44a7a1673ddbf667698cd05e13d1607c06f91527
Lucas Costa <88509075+lucascosta9349@users.noreply.github.com>
2024-01-02 11:34:05 -0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelector("#buttons-container button");
+
+class calculator{}
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) =>{
+
+        const value = e.target.innerText;
+
+        console.log(value);
+    });
+});
